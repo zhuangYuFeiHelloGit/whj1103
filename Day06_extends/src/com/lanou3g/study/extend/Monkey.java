53937e80d6b35eb0415f23a5582970e4afc072ca
zyf <zyf@zyfdeMBP.lan>
2017-11-29 16:30:34 +0800

    第六天课程结束：super关键字和子父类构造方法执行顺序

---
+++ Day06_extends/src/com/lanou3g/study/extend/Monkey.java
@@ -0,0 +1,28 @@
+package com.lanou3g.study.extend;
+
+public class Monkey extends Animal{
+
+	public Monkey() {
+		//super()就是在调用父类的空参的构造方法
+		//super("猴子")就是在
+		//调用父类的参数类型为String的构造方法
+		super("猴子");
+		System.out.println("i am a 齐天大圣");
+	}
+
+	@Override
+	public void sleep() {
+		//super.sleep()是在
+		//调用父类的sleep方法
+		System.out.println("我是猴子的睡觉方法");
+		super.sleep();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
